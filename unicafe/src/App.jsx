b52938886e78b0891417a34dd5c3d2eda5d12b4a
Martin Kobro <loellis92@gmail.com>
2024-01-12 12:02:10 +0100

    Exercise 1.6 + 1.7: Feedback buttons + stastistics.

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [posPercent, setPositivePercentage] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(allClicks + 1)
+    calculateStatistics(good+1, bad, allClicks+1)
+  }
+  const handleNeutralClick = () => {
+    setAll(allClicks + 1)
+    setNeutral(neutral + 1)
+    calculateStatistics(good, bad, allClicks+1)
+  }
+  const handleBadClick = () => {
+    setAll(allClicks + 1)
+    setBad(bad + 1)
+    calculateStatistics(good, bad+1, allClicks+1)
+  }
+
+  const calculateStatistics = (good, bad, allClicks) => {
+    setAverage((good - bad) / allClicks);
+    setPositivePercentage((good / allClicks) * 100);
+  };
+
+
+  return (
+    <div>
+      <div>
+        <h1>Give Feedback</h1>
+        <button onClick={handleGoodClick}>Good</button>
+        <button onClick={handleNeutralClick}>Neutral</button>
+        <button onClick={handleBadClick}>Bad</button>
+      </div>
+      <div>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {allClicks}</p>
+        <p>Average: {average}</p>
+        <p>Positive (%): {posPercent}%</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
